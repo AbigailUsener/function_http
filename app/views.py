06e71b9a218ef777bdd4e45f147181a9b9b6794b
Abigail Usener <ausener24@basecampcodingacademy.org>
2023-10-27 14:48:33 -0500

    finished

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+# Create your views here.
+def greeting(request: HttpRequest, name) -> HttpResponse:
+    names = (str(name))
+    return HttpResponse(f"Hello, {names}")
+def how_old(request: HttpRequest,end,birthyear) -> HttpResponse:
+    age=(end-birthyear)
+    return HttpResponse(age)
+def order(request: HttpRequest, burgers, fries, drinks) -> HttpResponse:
+    burger_price = 4.50
+    fries_price = 1.5
+    drinks_price = 1
+    burger_total = burgers * burger_price
+    fries_total = fries * fries_price
+    drinks_total = drinks * drinks_price
+    total = burger_total + fries_total + drinks_total
+    return HttpResponse(total)
---
+++ config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import greeting
+from app.views import how_old
+from app.views import order
+urlpatterns = [
+    path('hello/<name>', greeting),
+    path('how_old/<int:end>/<int:birthyear>',how_old),
+    path('order/<int:burgers>/<int:fries>/<int:drinks>',order),
+    path('admin/', admin.site.urls),
+]
